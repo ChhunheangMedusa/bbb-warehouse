34dbd6d1834f9ffe7968761c9066b4f21bb72837
ChhunheangMedusa <chhunheangmedusa@gmail.com>
2025-11-15 09:08:16 +0700

    update

--- includes/header.php
+++ includes/header.php
@@ -354,15 +354,13 @@ $hasAvatar = ($userPicture !== null);
                                 <li><a class="dropdown-item" href="../change-language.php?lang=km">ភាសាខ្មែរ</a></li>
                                 <li><a class="dropdown-item" href="../change-language.php?lang=en">English</a></li>
                             </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            
-                            <ul class=" nav-link dropdown-toggle" aria-labelledby="navbarDropdown">
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="../Admin/profile.php"><i class="bi bi-person me-2"></i><?php echo t('profile'); ?></a></li>
                                 <li><hr class="dropdown-divider"></li>
                                 <li><a class="dropdown-item" href="../logout.php"><i class="bi bi-box-arrow-right me-2"></i><?php echo t('logout'); ?></a></li>
                             </ul>
                         </li>
+                        
                     </ul>
                 </div>
             </div>
