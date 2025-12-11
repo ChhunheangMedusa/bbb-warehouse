011a7e35603f13819016cbbd09041689df3fb658
ChhunheangMedusa <chhunheangmedusa@gmail.com>
2025-12-11 13:47:49 +0700

    update

--- Staff/dashboard-staff.php
+++ Staff/dashboard-staff.php
@@ -5,7 +5,7 @@ require_once  'translate.php';
 
 if (!isStaff()) {
   $_SESSION['error'] = "You don't have permission to access this page";
-  header('Location: ../index.php');
+  header('Location: dashboard.php');
   exit();
 }
 
