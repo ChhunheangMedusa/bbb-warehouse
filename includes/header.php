dcafa1b06eebd3be5fb6c56e747451db04923e96
ChhunheangMedusa <chhunheangmedusa@gmail.com>
2025-12-11 08:51:03 +0700

    update

--- includes/header.php
+++ includes/header.php
@@ -384,8 +384,9 @@ $hasAvatar = ($userPicture !== null);
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="../Admin/profile.php"><i class="bi bi-person me-2"></i><?php echo t('profile'); ?></a></li>
+                                <li><a class="dropdown-item" href="select-destination.php"><i class="bi bi-person me-2"></i><?php echo t('profile'); ?></a></li>
                                 <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="../select-destination.php"><i class="bi bi-toggle me-2"></i><?php echo t('switch'); ?></a></li>
+                                
                                 <li><a class="dropdown-item" href="../logout.php"><i class="bi bi-box-arrow-right me-2"></i><?php echo t('logout'); ?></a></li>
                             </ul>
                         </li>
