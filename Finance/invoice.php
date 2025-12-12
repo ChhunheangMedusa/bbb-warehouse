e32da00bdf0fe3c3a03eed2e68d0d1af44ba3207
ChhunheangMedusa <chhunheangmedusa@gmail.com>
2025-12-12 09:02:01 +0700

    update

--- Finance/invoice.php
+++ Finance/invoice.php
@@ -3,7 +3,7 @@ ob_start();
 require_once '../config/database.php';
 require_once '../includes/functions.php';
 require_once '../includes/auth.php';
-require_once '../includes/header-finance.php';
+
 require_once 'translate.php';
 // Add authentication check
 //require_once '../includes/auth.php';
